71574bebc3646e68892f0c687f64f9f54558a84c
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-05-12 10:58:17 +0700

    menambahkan default

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -105,3 +105,13 @@ int main() {
         case 4:
             cout << "Exiting program." << endl;
             break;
+        
+        default:
+            cout << "invalid choice. Try again." << endl;
+            break;
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
