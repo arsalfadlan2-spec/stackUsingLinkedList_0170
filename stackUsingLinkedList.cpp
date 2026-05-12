6e0d74cc7ad04d4c8837a7e8fc48a0ca2d81c377
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-05-12 10:29:03 +0700

    first commit

---
+++ stackUsingLinkedList.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+//node class representating a single node in the linked list
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+//Stack class
+class stack
+{
+private:
