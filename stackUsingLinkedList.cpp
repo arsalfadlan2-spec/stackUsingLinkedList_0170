fc00118b1d815ce7250cd180a70d4214d41fd06e
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-05-12 10:57:26 +0700

    membuat int main

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -65,3 +65,18 @@ public:
         return top == NULL; // Return true if the top pointer is null
     }
 };
+
+int main() {
+    stack stack;
+    int choice = 0;
+    int value;
+
+    while (choice != 4) {
+        cout << "1.push\n";
+        cout << "2.pop\n";
+        cout << "3.peek\n";
+        cout << "4.Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
